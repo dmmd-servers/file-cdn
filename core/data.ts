3580fe9ef7a39ad5f43f09a44a3e9cd6c317b95a
DmmD GM <dm12332131mdgaming@gmail.com>
2025-05-27 10:18:52 -0400

    save

---
+++ core/data.ts
@@ -0,0 +1,16 @@
+// Imports
+export type 
+export class Token {
+    static generate(life: number): Token {
+
+    }
+    static list(): Token[] {
+
+    }
+    permit(access: Access):
+}
+export class Folder {
+    
+}
+export class File {
+}
