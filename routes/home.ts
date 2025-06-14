8ecc5cf50a2aa354359109788f76e8be8aedceb8
DmmD GM <dm12332131mdgaming@gmail.com>
2025-06-14 16:22:28 -0400

    added pathname check to home

--- routes/home.ts
+++ routes/home.ts
@@ -1,9 +1,15 @@
 // Imports
 import nodePath from "node:path";
 import direct from "../core/direct";
+import faults from "../core/faults";
 
 // Defines route
 export async function route(request: Request, server: Bun.Server): Promise<Response> {
+    // Parses url
+    const url = new URL(request.url);
+    const target = url.pathname.match(/^\/$/);
+    if(target === null) throw new faults.RouteAbort();
+
     // Resolves home
     const filepath = nodePath.resolve(direct.root, "./assets/html/index.html");
     const file = Bun.file(filepath);
