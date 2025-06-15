c116b23dd44ab7d7f6658dfffd1109e75d4fa427
DmmD GM <dm12332131mdgaming@gmail.com>
2025-06-15 16:51:49 -0400

    added files directory ensure

--- index.ts
+++ index.ts
@@ -1,8 +1,20 @@
 // Imports
+import nodeFile from "node:fs/promises";
 import chalk from "chalk";
 import access from "./core/access";
 import audit from "./core/audit";
+import direct from "./core/direct";
 import project from "./core/project";
+
+// Ensures files directory
+try {
+    audit("files", "Ensuring files directory...", chalk.yellow);
+    await nodeFile.mkdir(direct.files);
+    audit("files", "Files directory automatically created!", chalk.yellow);
+}
+catch {
+    audit("files", "Files directory detected!", chalk.green);
+}
 
 // Creates server
 const server = Bun.serve({
