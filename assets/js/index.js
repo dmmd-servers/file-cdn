b9cf0df81fc44586e9cf81bfd367708c469b3c04
DmmD GM <dm12332131mdgaming@gmail.com>
2025-06-15 17:18:40 -0400

    force double-slashes to be condensed

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@ if(elementList === null) throw new Error("Element (#list) not found!");
 // Defines dirname
 const parameters = new URLSearchParams(location.search);
 const query = parameters.get("q") ?? "/";
-const dirpath = new URL(query, location.origin).pathname;
+const dirpath = new URL(query, location.origin).pathname.replace(/\/{2,}/g, "/");
 
 // Fetches items
 const result = await fetch(`/d${dirpath}`);
@@ -20,7 +20,7 @@ if(!result.ok) elementList.innerText = "Something went wrong...";
 else for(let i = 0; i < filenames.length; i++) {
     const elementItem = document.createElement("a");
     const filename = filenames[i];
-    const filepath = new URL(dirpath + filename, location.origin).pathname;
+    const filepath = new URL(dirpath + filename, location.origin).pathname.replace(/\/{2,}/g, "/");
     elementItem.href = filename.endsWith("/") ? `/?q=${filepath}` : `/f${filepath}`;
     elementItem.innerText = filename;
     elementList.appendChild(elementItem);
